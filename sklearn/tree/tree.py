fix preg pointer access and path computation

sklearn/tree/tree.py
@@ -96,7 +96,7 @@ class BaseDecisionTree(six.with_metaclass(ABCMeta, BaseEstimator)):
                  min_impurity_split,
                  class_weight=None,
                  presort=False,
-                 P_reg=None):
+                 P_reg=None, tol=None):
         self.criterion = criterion
         self.splitter = splitter
         self.max_depth = max_depth
@@ -112,6 +112,7 @@ class BaseDecisionTree(six.with_metaclass(ABCMeta, BaseEstimator)):
         self.presort = presort
 
         self.P_reg = P_reg
+        self.tol = tol
 
     def fit(self, X, y, sample_weight=None, check_input=True,
             X_idx_sorted=None):
@@ -354,8 +355,18 @@ class BaseDecisionTree(six.with_metaclass(ABCMeta, BaseEstimator)):
                                                 random_state,
                                                 self.presort)
 
+
+        # for probabilistic tree
+        if self.tol is None:
+            self.tol = np.ones(self.n_features_) * 0.5
+        elif not isinstance(self.tol, np.ndarray):
+            self.tol = np.ones(self.n_features_) * self.tol
+
+
         self.tree_ = Tree(self.n_features_, self.n_classes_,
-                          n_samples, self.n_outputs_)
+                          n_samples, self.n_outputs_, self.tol)
+
+
 
         # Use BestFirst if max_leaf_nodes given; use DepthFirst otherwise
         if max_leaf_nodes < 0:
@@ -404,6 +415,8 @@ class BaseDecisionTree(six.with_metaclass(ABCMeta, BaseEstimator)):
         check_is_fitted(self, 'tree_')
         X = self._validate_X_predict(X, check_input)
         n_samples = X.shape[0]
+        n_features = X.shape[1]
+
 
         proba = self.tree_.predict2(X)
         return proba
@@ -1103,7 +1116,7 @@ class DecisionTreeRegressor(BaseDecisionTree, RegressorMixin):
                  max_leaf_nodes=None,
                  min_impurity_decrease=0.,
                  min_impurity_split=None,
-                 presort=False):
+                 presort=False, tol=None):
         super(DecisionTreeRegressor, self).__init__(
             criterion=criterion,
             splitter=splitter,
@@ -1116,7 +1129,7 @@ class DecisionTreeRegressor(BaseDecisionTree, RegressorMixin):
             random_state=random_state,
             min_impurity_decrease=min_impurity_decrease,
             min_impurity_split=min_impurity_split,
-            presort=presort)
+            presort=presort, tol=tol)
 
     def fit(self, X, y, sample_weight=None, check_input=True,
             X_idx_sorted=None):

test.py
@@ -9,16 +9,18 @@ print(sklearn.__path__)
 
 # @Warning: don't work with sparse matrix, not implemented for now.
 X = [[0, 0], [2, 2], [4,4]]
+#X = [[0], [2], [4]]
 Y = [0.5, 2.5, 4.5]
 #Y = [[0.5,0.6], [2.5,2.3], [4.5, 2.2]]
-sigmas = np.random.random(2) * (5 - 0) + 0
+sigmas = 0.00000001
 
 print('data X', X)
 print('Out Y', Y)
 
 
 reg = tree.DecisionTreeRegressor(criterion='mse2',
-                                 splitter='best2')
+                                 splitter='best2',
+                                 tol=sigmas)
 reg = reg.fit(X, Y)
 
 t = reg.tree_
@@ -36,10 +38,17 @@ print('Preg', t.preg)
 print(reg)
 
 new = [[0.4, 0.7],
-       [2.1,2.2],
-       [10,10],
-       [0,0]
+       [2.1, 2.2],
+       [10,  10],
+       [0,   0]
       ]
+
+#new = [[0.4],
+#       [2.1],
+#       [10],
+#       [0],
+#      ]
+
 
 print('data new', new)
 print('predict 1:', reg.predict(new))

test2.py
@@ -10,6 +10,8 @@ print(sklearn.__path__)
 if __name__ == '__main__':
 
     # Gen data
+    #sigmas = 0.00000001
+    sigmas = 0.1
     X = np.sort(5 * np.random.rand(80, 1), axis=0)
     y = np.sin(X).ravel()
     #X = np.asarray((X[:,0],X[:,0]+1)).T
@@ -24,11 +26,13 @@ if __name__ == '__main__':
     # Fit
     regr_1 = tree.DecisionTreeRegressor(criterion='mse2',
                                         splitter='best2',
-                                        max_depth=max_depth)
+                                        max_depth=max_depth,
+                                        tol=sigmas)
     regr_1.fit(X, y)
 
     # Predict
-    X_test = np.arange(0.0, 5.0, 0.05)[:, np.newaxis]
+    #X_test = np.arange(0.0, 5.0, 0.05)[:, np.newaxis]
+    X_test = X
     #X_test = np.asarray((X_test[:,0],X_test[:,0]+1)).T
     y_1 = regr_1.predict(X_test)
 